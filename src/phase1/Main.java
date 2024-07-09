9a96f4b422299482963732f766695ea129edc766
4rsh1y4 <arshiyanoroozi414@gmail.com>
2024-07-09 19:42:59 +0330

    updated

--- src/phase1/Main.java
+++ src/phase1/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("/FXML/mainMenu.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/FXML/logIn.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setResizable(false);
