9e22709515c77aead1e9c371540f0a50d2261e5d
Infamous <mohammadrezadar@gmail.com>
2024-07-11 01:43:09 +0430

    hey2

--- src/FXML/ModChooseController.java
+++ src/FXML/ModChooseController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import phase1.*;
 public class ModChooseController {
 
@@ -20,7 +21,8 @@ public class ModChooseController {
     private TextField numberInput;
     @FXML
     private Label warningLabel;
-
+    int betValue = -1;
+    int mod = -1;
     @FXML
     private Button backButton;
     @FXML
@@ -37,4 +39,45 @@ public class ModChooseController {
             e.printStackTrace();
         }
     }
+    @FXML
+    private void handleImageClick(MouseEvent event) {
+        ImageView clickedImageView = (ImageView) event.getSource();
+        if (clickedImageView == imageView1) {
+            mod = 1;
+        } else if (clickedImageView == imageView2) {
+            mod = 2;
+        }
+    }
+    @FXML
+    private void handleNextButton() {
+        if((mod ==2 && betValue!=-1)|| mod==1)
+        {try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Game.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) nextButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        }
+    }
+    @FXML
+    private void initialize() {
+        numberInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                int value = Integer.parseInt(newValue);
+                if (value < 50) {
+                    warningLabel.setText("Not Enough money");
+                    warningLabel.setVisible(true);
+                } else {
+                    warningLabel.setVisible(false);
+                }
+            } catch (NumberFormatException e) {
+                warningLabel.setText("Please enter a valid number");
+                warningLabel.setVisible(true);
+            }
+        });
+    }
+
 }
